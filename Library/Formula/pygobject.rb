1bb88359af94d0f11220cdd7b2949499388133ba
Anders F Bjorklund <afb@users.sourceforge.net>
2012-01-19 20:44:59 +0100

    disable introspection

--- Library/Formula/pygobject.rb
+++ Library/Formula/pygobject.rb
@@ -6,17 +6,11 @@ class Pygobject <Formula
   sha256 '5554acff9c27b647144143b0459359864e4a6f2ff62c7ba21cf310ad755cf7c7'
 
   depends_on 'pkg-config' => :build
-  depends_on 'libffi'
   depends_on 'glib'
-  depends_on 'gobject-introspection'
   depends_on 'pycairo'
 
   def install
-    ENV['FFI_CFLAGS'] = '-I/usr/include/ffi'
-    ENV['FFI_LIBS'] = '-L/usr/lib -lffi'
-    system "./configure", "--prefix=#{prefix}"
-    inreplace 'pygobject-2.0.pc', 'Requires.private: libffi', 'Libs.private: -lffi'
-    inreplace 'pygobject-2.0.pc', 'Cflags: ', 'Cflags: -I/usr/include/ffi '
+    system "./configure", "--prefix=#{prefix}", "--disable-introspection"
     system "make install"
   end
 end
