22504eff0bdfcca754d44c9a02c8b1d3bd375c1d
Anders F Bjorklund <afb@users.sourceforge.net>
2013-11-10 20:18:09 +0100

    Fix path to share, make sure to use /usr libiconv

--- Library/Formula/rpm.rb
+++ Library/Formula/rpm.rb
@@ -45,8 +45,9 @@ class Rpm < Formula
         --prefix=#{prefix}
         --localstatedir=#{var}
         --with-path-cfg=#{etc}/rpm
-        --with-path-magic=#{share}/misc/magic
+        --with-path-magic=#{HOMEBREW_PREFIX}/share/misc/magic
         --with-extra-path-macros=#{lib}/rpm/macros.*
+        --with-libiconv-prefix=/usr
         --disable-openmp
         --disable-nls
         --disable-dependency-tracking
